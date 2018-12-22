5cd75c3598b752792c41cd2ee21e37cfd0c379b8
Feiyu <feiyu_chen@g.harvard.edu>
2018-12-21 21:05:55 -0500

    Modified inline styles.

--- learn-dash/app.py
+++ learn-dash/app.py
@@ -7,12 +7,25 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Hello Dash',
+            style={
+                    'textAlign': 'center',
+                    'color': colors['text']
+                }),
 
     html.Div(children='''
         Dash: A web application framework for Python. Let's do some hot-reloading...
-    '''),
+    ''', 
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
 
     dcc.Graph(
         id='example-graph',
@@ -22,7 +35,11 @@ app.layout = html.Div(children=[
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
             }
         }
     )
