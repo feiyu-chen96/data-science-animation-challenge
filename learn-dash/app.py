c6780c36f53b84e37b8a687e301f565974e6087f
Feiyu <feiyu_chen@g.harvard.edu>
2018-12-21 21:08:08 -0500

    Displayed a Table.

--- learn-dash/app.py
+++ learn-dash/app.py
@@ -1,48 +1,34 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
+    'usa-agricultural-exports-2011.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash',
-            style={
-                    'textAlign': 'center',
-                    'color': colors['text']
-                }),
-
-    html.Div(children='''
-        Dash: A web application framework for Python. Let's do some hot-reloading...
-    ''', 
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
 ])
 
 if __name__ == '__main__':
